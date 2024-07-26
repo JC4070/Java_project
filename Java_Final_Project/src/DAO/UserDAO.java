c8c21bb18066048a85a440ceb723b8659f0b9200
Jatin <jatinchugh283@gmail.com>
2024-07-25 22:49:06 -0400

    jatin

--- Java_Final_Project/src/DAO/UserDAO.java
+++ Java_Final_Project/src/DAO/UserDAO.java
@@ -5,7 +5,9 @@ package DAO;
  */
 public interface UserDAO {
 
-	public boolean isRegistered(String UserName);
+	 public String registerUser(String username,String firstName,String lastName, String password, String userType);
+	 public boolean isRegistered(String UserName);
 	 public String isValidCredential(String UserName, String password);
 	 public String getName(String UserName);
+	 
 }
