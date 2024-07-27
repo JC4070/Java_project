09b3907b615bc83be75e15986bc22a35c02ca609
Jatin <jatinchugh283@gmail.com>
2024-07-27 00:06:29 -0400

    jatin

--- Java_Final_Project/src/DAO/ProductDAOImpl.java
+++ Java_Final_Project/src/DAO/ProductDAOImpl.java
@@ -1,12 +1,35 @@
 package DAO;
 
+import java.sql.*;
 import bean.Productbean;
 
 /**
  * @author Jatin
  */
 public class ProductDAOImpl implements ProductDAO {
+	private Connection connection;
+	
 	public Productbean getProductById(int productId) {
-		return product;}
+		 Productbean product = null;
+	        String sql = "SELECT * FROM Product WHERE product_id = ?";
+	        try (PreparedStatement stmt = connection.prepareStatement(sql)) {
+	            stmt.setInt(1, productId);
+	            ResultSet rs = stmt.executeQuery();
+	            if (rs.next()) {
+	                product = mapResultSetToProduct(rs);
+	            }
+	        } catch (SQLException e) {
+	            e.printStackTrace();
+	        }
+	        return product;
+	    }
+
+	public List<Productbean> getProductsByUserId(int userId) {
+        List<Productbean> productList = new ArrayList<>();
+        String sql = "SELECT * FROM Product WHERE user_id = ?";
+        try (PreparedStatement stmt = connection.prepareStatement(sql)) {
+            stmt.setInt(1, userId);
+            ResultSet rs = stmt.executeQuery();
+	
 	
 }
