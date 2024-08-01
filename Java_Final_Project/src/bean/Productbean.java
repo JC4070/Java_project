4d0adcba330675e3e20f85d8417fb085cbdfc59a
Raghav Chhabra <chha0030@algonquinlive.com>
2024-07-31 20:40:03 -0400

    Raghav Chhabra : Product Bean DTO

--- Java_Final_Project/src/bean/Productbean.java
+++ Java_Final_Project/src/bean/Productbean.java
@@ -5,54 +5,107 @@ import java.time.LocalDate;
 
 public class Productbean {
 
-	public void setProductName(String string) {
-		// TODO Auto-generated method stub
-		
-	}
+	    private int productId;
+	    private String productName;
+	    private String category;
+	    private String description;
+	    private String imageUrl;
+	    private BigDecimal sellerPrice;
+	    private LocalDate addedDate;
+	    private String foodCondition;
+	    private int userId;
 
-	public void setProductId(int int1) {
-		// TODO Auto-generated method stub
-		
-	}
 
-	public void setCategory(String string) {
-		// TODO Auto-generated method stub
-		
-	}
 
-	public void setDescription(String string) {
-		// TODO Auto-generated method stub
-		
-	}
+	    public Productbean(){
+	        
+	    }    
+	    
+	    public Productbean( String productName, String category, String description, BigDecimal sellerPrice, LocalDate addedDate, String foodCondition, int userId, String imageUrl) {
+	        this.productName = productName;
+	        this.category = category;
+	        this.description = description;
+	        this.sellerPrice = sellerPrice;
+	        this.addedDate = addedDate;
+	        this.foodCondition = foodCondition;
+	        this.userId = userId;
+	        this.imageUrl = imageUrl;
+	    }
 
-	public void setSellerPrice(BigDecimal bigDecimal) {
-		// TODO Auto-generated method stub
-		
-	}
+		public int getProductId() {
+			return productId;
+		}
 
-	public void setUserId(int int1) {
-		// TODO Auto-generated method stub
-		
-	}
+		public void setProductId(int productId) {
+			this.productId = productId;
+		}
 
-	public void setImageUrl(String string) {
-		// TODO Auto-generated method stub
-		
-	}
+		public String getProductName() {
+			return productName;
+		}
 
-	public void setFoodCondition(String string) {
-		// TODO Auto-generated method stub
-		
-	}
+		public void setProductName(String productName) {
+			this.productName = productName;
+		}
 
-	public void setAddedDate(LocalDate localDate) {
-		// TODO Auto-generated method stub
-		
-	}
+		public String getCategory() {
+			return category;
+		}
 
-	public String getProductName() {
-		// TODO Auto-generated method stub
-		return null;
-	}
+		public void setCategory(String category) {
+			this.category = category;
+		}
+
+		public String getDescription() {
+			return description;
+		}
+
+		public void setDescription(String description) {
+			this.description = description;
+		}
+
+		public BigDecimal getSellerPrice() {
+			return sellerPrice;
+		}
+
+		public void setSellerPrice(BigDecimal sellerPrice) {
+			this.sellerPrice = sellerPrice;
+		}
+
+		public LocalDate getAddedDate() {
+			return addedDate;
+		}
+
+		public void setAddedDate(LocalDate addedDate) {
+			this.addedDate = addedDate;
+		}
+
+		public String getFoodCondition() {
+			return foodCondition;
+		}
+
+		public void setFoodCondition(String foodCondition) {
+			this.foodCondition = foodCondition;
+		}
+		
+		public String getImageUrl() {
+			return imageUrl;
+		}
+
+		public void setImageUrl(String imageUrl) {
+			this.imageUrl = imageUrl;
+		}
+	    
+	    
+
+		public int getUserId() {
+			return userId;
+		}
+
+		public void setUserId(int userId) {
+			this.userId = userId;
+		}
+
+
 
 }
