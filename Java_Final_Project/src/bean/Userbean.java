8a4cde7233ae43ee2ab5ca5de23888d045b86aca
Raghav Chhabra <chha0030@algonquinlive.com>
2024-07-31 20:43:26 -0400

    Raghav Chhabra : User bean DTO

--- Java_Final_Project/src/bean/Userbean.java
+++ Java_Final_Project/src/bean/Userbean.java
@@ -1,5 +1,28 @@
 package bean;
 
+
 public class Userbean {
+	
+	 private int UserId;
+	    private String username;
+	    private String firstName;
+	    private String lastName;
+	    private String userType;
+	    private String password;
+
+	    
+	    public Userbean(){
+	        
+	    }
+
+	    public Userbean( String username,String firstName,String lastName,String password, String userType) {
+	        super();
+	        this.username =username;
+	        this.firstName=firstName;
+	        this.lastName=lastName;
+	        this.userType = userType;
+	        this.password = password;
+
+	    }
 
 }
