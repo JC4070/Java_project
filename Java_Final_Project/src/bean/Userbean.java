8bfd18b35cd669dde612afb65bacd039b8ab9c2a
Raghav Chhabra <chha0030@algonquinlive.com>
2024-07-31 20:43:48 -0400

    Raghav Chhabra : Product bean DTO

--- Java_Final_Project/src/bean/Userbean.java
+++ Java_Final_Project/src/bean/Userbean.java
@@ -25,4 +25,54 @@ public class Userbean {
 
 	    }
 
+		public int getUserId() {
+			return UserId;
+		}
+
+		public void setUserId(int userId) {
+			UserId = userId;
+		}
+
+		public String getUsername() {
+			return username;
+		}
+
+		public void setUsername(String username) {
+			this.username = username;
+		}
+
+		public String getFirstName() {
+			return firstName;
+		}
+
+		public void setFirstName(String firstName) {
+			this.firstName = firstName;
+		}
+
+		public String getLastName() {
+			return lastName;
+		}
+
+		public void setLastName(String lastName) {
+			this.lastName = lastName;
+		}
+
+		public String getUserType() {
+			return userType;
+		}
+
+		public void setUserType(String userType) {
+			this.userType = userType;
+		}
+
+		public String getPassword() {
+			return password;
+		}
+
+		public void setPassword(String password) {
+			this.password = password;
+		}
+	    
+	    
+
 }
